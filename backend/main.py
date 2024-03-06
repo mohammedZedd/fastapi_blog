c7cfa4a4e74f336df8951557c7837ca4910f77d4
Mohammed <mohammed.zeddaouii@gmail.com>
2024-03-06 13:28:47 +0100

    seconde commit

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.base import api_router
+# from db.base import Base
+# from db.session import engine
 
-app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+
+# def create_tables():
+#     Base.metadata.create_all(bind=engine)
+
+
+def include_router(app):
+    app.include_router(api_router)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+    # create_tables()
+    include_router(app)
+    return app
+
+app = start_application()
+    
 
 
 @app.get("/")
